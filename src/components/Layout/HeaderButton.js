0ca17e673046cfcb2bb3fa09551d3ae4f7e9d939
AbelTeha <abelteha@gmail.com>
2022-10-20 12:02:16 +0300

    added effect to the header button

--- src/components/Layout/HeaderButton.js
+++ src/components/Layout/HeaderButton.js
@@ -1,16 +1,28 @@
 import classes from "./HeaderButton.module.css";
 import CartContext from "../../store/cart-context";
 import { BsCartFill } from "react-icons/bs";
-import { useContext, useMemo } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 const HeaderButton = (props) => {
+  const [isAdded, setIsAdded] = useState(false);
   const cartCxt = useContext(CartContext);
+
+  useEffect(() => {
+    setIsAdded(true);
+    setTimeout(() => {
+      setIsAdded(false);
+    }, 300);
+  }, [cartCxt.items]);
+
   const numberOfItems = cartCxt.items.reduce(
     (total, item) => (total += item.amount),
     0
   );
   const icon = useMemo(() => <BsCartFill />, []);
   return (
-    <button className={classes["header-button"]} onClick={props.onClick}>
+    <button
+      className={`${classes["header-button"]} ${isAdded && classes.bump}`}
+      onClick={props.onClick}
+    >
       <span className={classes.icon}>{icon}</span>
       <span className={classes.title}>Your Cart</span>
       <span className={classes.badge}>{numberOfItems}</span>
